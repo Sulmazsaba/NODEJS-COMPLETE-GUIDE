e330e0c7c1af9b0c9907b70af3c04205d11e563e
Sulmaz <sulmazsaba@gmail.com>
2021-05-19 15:46:05 +0430

    initialized commit

---
+++ routes.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+  }
+};
+
+exports.handler = requestHandler;

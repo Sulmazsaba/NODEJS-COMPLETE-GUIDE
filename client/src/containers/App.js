7cc8ba951231d645a2a524d8d6d193c9d4fddd39
Sulmaz <sulmazsaba@gmail.com>
2021-05-26 17:53:17 +0430

    add layout

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,50 +1,59 @@
-import "./App.css";
-import Products from "../components/products/products";
+import Layout from "../components/Layout/Layout";
 import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("app js constructor");
-    this.state = {
-      products: [
-        {
-          id: 1,
-          title: "shoe",
-          imgUrl:"bag.jpg"
-        },
-        {
-          id: 2,
-          title: "bag",
-        },
-        {
-          id: 3,
-          title: "neckless",
-        },
-      ],
-      otherState: "some other value",
-      showProducts: false,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   console.log("app js constructor");
+  //   this.state = {
+  //     products: [
+  //       {
+  //         id: 1,
+  //         title: "shoe",
+  //         imgUrl:"bag.jpg"
+  //       },
+  //       {
+  //         id: 2,
+  //         title: "bag",
+  //       },
+  //       {
+  //         id: 3,
+  //         title: "neckless",
+  //       },
+  //     ],
+  //     otherState: "some other value",
+  //     showProducts: false,
+  //   };
+  // }
 
-  static getDerivedStateFormProps(props, state) {
-    console.log(props);
-    return state;
-  }
+  // static getDerivedStateFormProps(props, state) {
+  //   console.log(props);
+  //   return state;
+  // }
 
-  componentWillUnmount() {}
-  componentDidMount() {}
+  // componentWillUnmount() {}
+  // componentDidMount() {}
+  // render() {
+  //   let products = this.state.products;
+  //   if (this.state.products.length > 0) {
+  //     products = <Products products={products}
+  //     deleted={this.deleteProductHandler}
+  //     ></Products>;
+  //   }
+  //   return (
+  //     <div>
+  //       <h1>{this.props.appTitle}</h1>
+  //       {products}
+  //     </div>
+  //   );
+  // }
+
   render() {
-    let products = this.state.products;
-    if (this.state.products.length > 0) {
-      products = <Products products={products}
-      deleted={this.deleteProductHandler}
-      ></Products>;
-    }
     return (
       <div>
-        <h1>{this.props.appTitle}</h1>
-        {products}
+        <Layout>
+          <p>hello from layout</p>
+        </Layout>
       </div>
     );
   }
